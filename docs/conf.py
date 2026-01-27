5e6060d1d3054b5c25be3293dfbcb91d297fa54e
Michael Anderson <52722296+and02709@users.noreply.github.com>
2026-01-27 11:32:33 -0600

    Update author and copyright information in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 project = 'GDC'
-author = 'Michael'
-extensions = []
+copyright = '2026, Michael Anderson'
+author = 'Michael Anderson'
+extensions = ['sphinx_rtd_theme']
 html_theme = 'sphinx_rtd_theme'
