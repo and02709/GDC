a23122040fa77be656e2fd9537c146ccf0fef14f
Michael Anderson <52722296+and02709@users.noreply.github.com>
2026-01-28 13:25:07 -0600

    Add MathJax extension to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 project = 'GDC'
 copyright = '2026, Michael Anderson'
 author = 'Michael Anderson'
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme',
+             'sphinx.ext.mathjax']
 html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
